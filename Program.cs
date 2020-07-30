928426b9d2210cf50b1ebe02a722fe3c994b6c46
Marc-Andrea Klimaschewski <marcklim@github.com>
2020-07-30 15:18:48 +0000

    making a hello world app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace msbuild_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
